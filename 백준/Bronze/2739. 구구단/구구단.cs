5a5071465578ac6b3a0ce4ec522e2686c3c75bea
CheolYee <81477259+CheolYee@users.noreply.github.com>
2025-10-03 04:13:12 +0900

    [Bronze V] Title: 구구단, Time: 48 ms, Memory: 5824 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine($"{N} * {i} = {N*i}");
+            }
+        }
+    }
+}
