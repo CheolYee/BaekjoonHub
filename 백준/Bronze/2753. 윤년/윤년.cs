ed2271b7c6c9e2832a20cad15c20f263d7c2260c
CheolYee <81477259+CheolYee@users.noreply.github.com>
2025-10-03 04:28:02 +0900

    [Bronze V] Title: 윤년, Time: 52 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackJoon3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            
+            if (N % 400 == 0 || (N % 4 == 0 && N % 100 != 0))
+            {
+                Console.WriteLine(1);
+            }
+            else
+                Console.WriteLine(0);
+        }
+    }
+}
